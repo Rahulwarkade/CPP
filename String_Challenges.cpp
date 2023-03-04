6b1b2d996c0ff8e22ca3221cf756ead90047779b
Arule <88571832+Rahulwarkade@users.noreply.github.com>
2023-03-04 07:12:59 +0530

    String challenges

---
+++ String_Challenges.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+// #include<iostream>
+// #include<string>
+// #include<algorithm>
+using namespace std;
+int main()
+{
+  // UPPER CASE AND LOWER CASE CONVERTER
+  // string str;
+  // getline(cin,str);
+  string s;
+  getline(cin,s);
+  // for(int i=0; i<str.size(); i++)
+  //   {
+  //     str[i] += 32;
+  //   }
+  // cout<<str<<endl;
+  // for(int i=0; i<str.size(); i++)
+  //   {
+  //     str[i] -= 32;
+  //   }
+  // cout<<str<<endl;
+  // transform(str.begin(),str.end(),str.begin(), ::toupper);
+  // cout<<str<<endl;
+  // transform(str.begin(),str.end(),str.begin(), ::tolower);
+  // cout<<str<<endl;
+
+  // BIGGEST NUMBER
+  // string s = "2342345234";
+  // sort(s.begin(),s.end(),greater<char>());
+  // cout<<s<<endl;
+
+  // FIND THE FREQUENCY OF ANY CHARACTER
+
+  int freq[26];
+  for(int i=0; i<26; i++)
+    freq[i] =0;
+
+  for(int i=0; i<s.length(); i++)
+    {
+      if(s.at(i)>='a' && s.at(i)<='z')
+        freq[s[i]-'a']++;
+    }
+  char ans = 'a';
+  int mxFre = 0;
+  for(int i=0; i<26; i++)
+    {
+      if(freq[i]>mxFre)
+      {
+        mxFre = freq[i];
+        ans = i+'a';
+      }
+    }
+  cout<<mxFre<<" "<<ans<<endl;
+}
