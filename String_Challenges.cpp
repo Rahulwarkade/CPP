91579282762f124f54a5ccfb6ccb5e8752e1ce08
RAHUL WARKADE <88571832+Rahulwarkade@users.noreply.github.com>
2023-03-24 23:59:48 +0530

    Update String_Challenges.cpp

--- String_Challenges.cpp
+++ String_Challenges.cpp
@@ -10,25 +10,25 @@ int main()
   // getline(cin,str);
   string s;
   getline(cin,s);
-  // for(int i=0; i<str.size(); i++)
-  //   {
-  //     str[i] += 32;
-  //   }
-  // cout<<str<<endl;
-  // for(int i=0; i<str.size(); i++)
-  //   {
-  //     str[i] -= 32;
-  //   }
-  // cout<<str<<endl;
+  for(int i=0; i<str.size(); i++)
+    {
+      str[i] += 32;
+    }
+  cout<<str<<endl;
+  for(int i=0; i<str.size(); i++)
+    {
+      str[i] -= 32;
+    }
+  cout<<str<<endl;
   // transform(str.begin(),str.end(),str.begin(), ::toupper);
   // cout<<str<<endl;
   // transform(str.begin(),str.end(),str.begin(), ::tolower);
   // cout<<str<<endl;
 
   // BIGGEST NUMBER
-  // string s = "2342345234";
-  // sort(s.begin(),s.end(),greater<char>());
-  // cout<<s<<endl;
+  string s = "2342345234";
+  sort(s.begin(),s.end(),greater<char>());
+  cout<<s<<endl;
 
   // FIND THE FREQUENCY OF ANY CHARACTER
 
